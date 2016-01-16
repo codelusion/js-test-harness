7d46571cd7bbcecd93bc336d70ae9712fc46d4c8
codelusion <ash@aisaac.io>
2016-01-15 22:36:22 -0600

    Adding Specs, setup gulp tasks

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 js-test-harness
 
 A test harness for Javascript using Gulp, Karma, Jasmine and Phantom.js
+
+1) Setting up Karma for TDD, using multiple browsers
+2) Setting up Karma for CI/Build Server
+3) Setting up test filters
+4) Organizing test suites for ease of use / isolated testing
+5) Testing Ajax
+6) jQuery with HTML injection
+
---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+'use strict';
+var gulp = require('gulp');
+var Server = require('karma').Server;
+var env = require('gulp-env');
+var testSetup = require('./lib/test-setup');
+var argv = require('minimist')(process.argv.slice(2));
+var testResults = require('./lib/test-report');
+
+//setup environment variables
+gulp.task('set-env', function () {
+    env({
+        vars: {
+            //add path to browser binaries Phantom, Chrome, IE etc
+            //http://karma-runner.github.io/0.13/config/browsers.html
+            PHANTOMJS_BIN: __dirname + "/node_modules/phantomjs/lib/phantom/bin/phantomjs"
+        }
+    })
+});
+
+
+//test runner task
+gulp.task('karma-test-js', ['set-env'], function (done) {
+    var clientArgs = [];
+    //capture filters if passed in via command line
+    if (argv.grep || argv.f) {
+        clientArgs = ['--grep', argv.grep || argv.f];
+    }
+    //init Karma server instance
+    //handle exit status
+    new Server({
+        basePath: __dirname,
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true,
+        files: testSetup.includeFiles(),
+        exclude: testSetup.excludeFiles(),
+        client: {
+            args: clientArgs
+        }
+    }, function (exitStatus) {
+        testResults.save(exitStatus);
+        done();
+    }).start();
+});
+
+//watch files and run tests on change
+gulp.task('tdd-watch', ['set-env'], function (done) {
+    new Server({
+        basePath: __dirname + '/../',
+        configFile: __dirname + '/karma.conf.js',
+        files: testSetup.includeFiles(),
+        exclude: testSetup.excludeFiles(),
+        autoWatch: true
+    }, done).start();
+});
+
+
+//final report on tests
+//exit code set here for CI/Build server
+gulp.task('final', ['karma-test-js'], function () {
+    testResults.report();
+});
+
+//Task dependencies
+gulp.task('test', ['set-env', 'karma-test-js', 'final']);
+gulp.task('tdd', ['set-env', 'tdd-watch']);
